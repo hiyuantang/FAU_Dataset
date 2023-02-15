5489cb2db3b305be3ef856a9552c8365025186e4
Yuan Tang <holatangyuan@gmail.com>
2023-02-14 18:55:28 -0800

    Update derivative.py

--- code/derivative.py
+++ code/derivative.py
@@ -15,3 +15,30 @@ import argparse
 from vgg_face import *
 from FAUDataset import *
 
+
+num_classes = 10
+batch_size = 32
+checkpoint_path = 'D:/FAU_models/models_r16/checkpoint_epoch49.pth'
+dataset_root = 'D:/Datasets/FAU'
+
+data_transforms = {
+        'test': transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            #transforms.Normalize([0.3873985 , 0.42637664, 0.53720075], [0.2046528 , 0.19909547, 0.19015081])
+        ]),
+    }
+
+def set_parameter_requires_grad(model, feature_extracting):
+    for param in model.parameters():
+        param.requires_grad = False if feature_extracting else True
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
+
+model = VGG_16()
+set_parameter_requires_grad(model, feature_extracting=False)
+num_ftrs = model.fc8.in_features
+model.fc8 = nn.Linear(num_ftrs, num_classes)
+model.load_state_dict(torch.load(checkpoint_path), strict=False)
