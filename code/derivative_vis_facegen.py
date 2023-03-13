a1c3e949b09e0d860277be22cf7108cb75a53ff0
Yuan Tang <hiyuantang@gmail.com>
2023-03-12 21:30:32 -0700

    Update derivative_vis_facegen.py

--- code/derivative_vis_facegen.py
+++ code/derivative_vis_facegen.py
@@ -10,19 +10,24 @@ import matplotlib.pyplot as plt
 from vgg_face import *
 from FAUDataset import *
 
-# sample running command: 
-# python derivative_vis_facegen.py --gender man --seed 66 --activation 5 --au 4
+# dAU7/Delta face change  as \sum dAU7/dfeatureact * Delta featureact/Delta face change 
 
-parser = argparse.ArgumentParser(description='d AU# / d input')
+# sample running command: 
+# python derivative_func.py --gender man --seed 66 --activation 5 --au 4
+
+parser = argparse.ArgumentParser(description='d AU# / d face change')
 parser.add_argument('--au', default='4', type=str, help='select an au number from [4,6,7,10,12,20,25,26,43]')
 parser.add_argument('--gender', default='man', type=str, help='select a gender from [man, woman]')
 parser.add_argument('--seed', default='66', type=str, help='select a random seed from [16, 66]')
-parser.add_argument('--activation', default=5, type=int, help='select a random seed from [0,1,2,3,4,5]')
+parser.add_argument('--activation', default=5, type=int, help='select an activation level from [0,1,2,3,4,5]')
+parser.add_argument('--featureL', default=0, type=int, help='select a feature layer from 0 to 15')
 args = parser.parse_args()
+
 
 num_classes = 10
 batch_size = 32
-checkpoint_path = '/Volumes/Yuan-T7/FAU_models/models_r'+args.seed+'/checkpoint_epoch49.pth'
+#checkpoint_path = '/Volumes/Yuan-T7/FAU_models/models_r'+args.seed+'/checkpoint_epoch49.pth'
+checkpoint_path = '/Volumes/Yuan-T7/FAU_models/checkpoint_epoch_init.pth'
 root_path = '/Volumes/Yuan-T7/Datasets/FAU/images'
 
 if args.gender == 'man': 
@@ -42,8 +47,13 @@ else:
         image_path_0 = os.path.join(gender_path, 'ew5/'+args.au+'ew5_'+args.au+'.'+str(args.activation*2)+'.png')
         image_path_1 = os.path.join(gender_path, 'ew6/'+args.au+'ew6_'+args.au+'.'+str(args.activation*2)+'.png')
 
+# choose index from 2 to inf
+def array_repeat(x, index):
+    x = np.repeat(x, index, axis = 0)
+    x = np.repeat(x, index, axis = 1)
+    return x
 
-def cal_derivative(image_path):
+def cal_derivative(image_path, cut):
     data_transforms = {
             'test': transforms.Compose([
                 transforms.ToPILImage(),
@@ -60,137 +70,106 @@ def cal_derivative(image_path):
     num_ftrs = model.fc8.in_features
     model.fc8 = nn.Linear(num_ftrs, num_classes)
     model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('mps')), strict=False)
-
-    model.train()
-
-    input_image = Image.open(image_path)
-    input_image = input_image.convert('RGB')
-    input_image = torch.tensor(np.array(input_image))
-    input_image = torch.transpose(input_image, 0, 2).transpose(1, 2)
-    input_image = input_image[:, 200:1000, 850:1650]
-    input_image = data_transforms['test'](input_image)
-    input_image = input_image.view(1, input_image.shape[0], input_image.shape[1], input_image.shape[2])
-    input_image.requires_grad = True
-
     model.to('cpu')
-    input_image.to('cpu')
-    output = model(input_image)
-    loss = torch.sum(output)
 
+    model.eval()
+    with torch.no_grad():
+        input_image = Image.open(image_path)
+        input_image = input_image.convert('RGB')
+        input_image = torch.tensor(np.array(input_image)).to('cpu')
+        input_image = torch.transpose(input_image, 0, 2).transpose(1, 2)
+        input_image = input_image[:, 200:1000, 850:1650]
+        input_image = data_transforms['test'](input_image)
+        input_image = input_image.view(1, input_image.shape[0], input_image.shape[1], input_image.shape[2])
+        input_inter = model.forward_thoughts(input_image, cut)
+    
+    model.train()
+    input_inter.requires_grad = True
+
+    if cut == 0:
+        output = model.forward_0(input_inter)
+    elif cut == 1:
+        output = model.forward_1(input_inter)
+    elif cut == 2:
+        output = model.forward_2(input_inter)
+    elif cut == 3:
+        output = model.forward_3(input_inter)
+    elif cut == 4:
+        output = model.forward_4(input_inter)
+    au_list = ['pspi', '4', '6', '7', '10', '12', '20', '25', '26', '43']
+    loss = output[0, au_list.index(args.au)]
     loss.backward(retain_graph=True)
-    feature_derivative = input_image.grad
+    feature_derivative = input_inter.grad
 
-    feature_derivative = torch.squeeze(feature_derivative)
-    array = feature_derivative.permute(1, 2, 0).cpu().numpy()
+    #feature_derivative = torch.squeeze(feature_derivative)
+    #darray = feature_derivative.permute(1, 2, 0).cpu().numpy()
     
     input_image = torch.squeeze(input_image)
     input_image = input_image.permute(1, 2, 0).cpu().detach().numpy()
+    input_inter = input_inter.cpu().detach().numpy()
+
     output = output.cpu().detach().numpy()
 
-    return array, input_image, output
+    return feature_derivative, input_image, input_inter, output
 
-def cal_derivative_facegen(image_path):
-    data_transforms = {
-            'test': transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-                #transforms.Normalize([0.3873985 , 0.42637664, 0.53720075], [0.2046528 , 0.19909547, 0.19015081])
-            ]),
-        }
+fig, axs = plt.subplots(3, 5, figsize=(13, 8))
+for cut in range(5):
+    darray_light, input_image_light, inter_light, output_light = cal_derivative(image_path_0, cut)
+    darray_dark, input_image_dark, inter_dark, output_dark = cal_derivative(image_path_1, cut)
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu')
-    
-    model = VGG_16()
-    num_ftrs = model.fc8.in_features
-    model.fc8 = nn.Linear(num_ftrs, num_classes)
-    model.load_state_dict(torch.load(checkpoint_path, map_location=torch.device('mps')), strict=False)
+    dfeature = inter_light - inter_dark
 
-    model.train()
+    dau_dface_light = torch.squeeze(darray_light * dfeature)
+    dau_dface_dark = torch.squeeze(darray_dark * dfeature)
 
-    input_image = Image.open(image_path)
-    input_image = input_image.convert('RGB')
-    input_image = torch.tensor(np.array(input_image))
-    input_image = torch.transpose(input_image, 0, 2).transpose(1, 2)
-    input_image = data_transforms['test'](input_image)
-    input_image = input_image.view(1, input_image.shape[0], input_image.shape[1], input_image.shape[2])
-    input_image.requires_grad = True
-
-    model.to('cpu')
-    input_image.to('cpu')
-    output = model(input_image)
-    # loss = torch.sum(output)
-    # loss = output[#]
-
-    loss.backward(retain_graph=True)
-    feature_derivative = input_image.grad
-
-    feature_derivative = torch.squeeze(feature_derivative)
-    array = feature_derivative.permute(1, 2, 0).cpu().numpy()
-    
-    input_image = torch.squeeze(input_image)
-    input_image = input_image.permute(1, 2, 0).cpu().detach().numpy()
-    output = output.cpu().detach().numpy()
-
-    return array, input_image, output
+    dau_dface_light = np.sum(dau_dface_dark.numpy(), axis=0)
+    dau_dface_dark= np.sum(dau_dface_dark.numpy(), axis=0)
 
 
+    normalized_light = np.interp(dau_dface_light, (dau_dface_light.min(), dau_dface_light.max()), (0, 1))
+    normalized_light = normalized_light - np.mean(normalized_light)
+    normalized_light[normalized_light < 0] = 0
+    normalized_light = array_repeat(normalized_light, 2**(cut+1))
+    normalized_dark = np.interp(dau_dface_dark, (dau_dface_dark.min(), dau_dface_dark.max()), (0, 1))
+    normalized_dark = normalized_dark - np.mean(normalized_dark)
+    normalized_dark[normalized_dark < 0] = 0
+    normalized_dark = array_repeat(normalized_dark, 2**(cut+1))
 
-darray_light, input_image_light, output_light = cal_derivative(image_path_0)
-darray_dark, input_image_dark, output_dark = cal_derivative(image_path_1)
+    axs[0,cut].imshow(normalized_light, cmap='Greys')
+    axs[0,cut].set_title('hidden_state'+str(cut))
 
-facegen_path = '/Volumes/Yuan-T7/Datasets/Face_gen/9.19/164e_7.100.png'
-darray_facegen, input_facegen, output_facegen = cal_derivative_facegen(facegen_path)
+    #axs[0,1].imshow(normalized_dark, cmap='Greys')
+    #axs[0,1].set_title('ddark_au / dface')
 
-darray_light = np.sum(darray_light, axis=2)
-darray_dark = np.sum(darray_dark, axis=2)
-darray_facegen = np.sum(darray_facegen, axis=2)
+    axs[1,cut].imshow(input_image_light+np.tile(normalized_light, (3,1,1)).transpose(1,2,0))
+    #axs[1,1].imshow(input_image_dark+np.tile(normalized_dark, (3,1,1)).transpose(1,2,0))
 
-normalized_light = np.interp(darray_light, (darray_light.min(), darray_light.max()), (0, 1))
-normalized_light = normalized_light - np.mean(normalized_light)
-normalized_light[normalized_light < 0] = 0
-normalized_dark = np.interp(darray_dark, (darray_dark.min(), darray_dark.max()), (0, 1))
-normalized_dark = normalized_dark - np.mean(normalized_dark)
-normalized_dark[normalized_dark < 0] = 0
-normalized_facegen = np.interp(darray_facegen, (darray_facegen.min(), darray_facegen.max()), (0, 1))
-normalized_facegen = normalized_facegen - np.mean(normalized_facegen)
-normalized_facegen[normalized_facegen < 0] = 0
+    if cut == 0:
+        axs[2,cut].imshow(input_image_light)
+        #axs[2,1].imshow(input_image_dark)
+    else:
+        pass
 
-fig, axs = plt.subplots(3, 3, figsize=(10, 8))
-
-axs[0,0].imshow(normalized_light, cmap='Greys')
-axs[0,0].set_title('light')
-
-axs[0,1].imshow(normalized_dark, cmap='Greys')
-axs[0,1].set_title('dark')
-
-im = axs[0,2].imshow(normalized_facegen, cmap='Greys')
-axs[0,2].set_title('facegen')
-fig.colorbar(im, ax=axs[0,2])
-
-axs[1,0].imshow(input_image_light+np.tile(normalized_light, (3,1,1)).transpose(1,2,0))
-axs[1,1].imshow(input_image_dark+np.tile(normalized_dark, (3,1,1)).transpose(1,2,0))
-axs[1,2].imshow(input_facegen+np.tile(normalized_facegen, (3,1,1)).transpose(1,2,0))
-
-axs[2,0].imshow(input_image_light)
-axs[2,1].imshow(input_image_dark)
-
+##########################
 y = [0,1,2,3,4,5,6,7,8,9]
-axs[2,2].plot(y, output_light.reshape(10,1), marker = 'o', c = '#f7ead0')
-axs[2,2].plot(y, output_dark.reshape(10,1), marker = 'o', c = '#3a312a')
-axs[2,2].plot(y, output_facegen.reshape(10,1), marker = 'o', c = '#1560bd')
+axs[2,1].plot(y, output_light.reshape(10,1), marker = 'o', c = '#f7ead0')
+axs[2,1].plot(y, output_dark.reshape(10,1), marker = 'o', c = '#3a312a')
+
+axs[2,2].axis('off')
+axs[2,3].axis('off')
+axs[2,4].axis('off')
+##########################
 
 for i, ax in enumerate(axs.flat):
     ax.set_xticks([])
     ax.set_yticks([])
-    if i == 8:
+    if i == 11:
         ax.set_xticks(np.arange(10), ['pspi', '4', '6', '7', '10', '12', '20', '25', '26', '43'])
         ax.set_yticks([0,1])
 
 if args.activation == 0:
-    fig.suptitle('model '+'r'+args.seed+'_activation at '+str(args.activation*20)+'%')
+    fig.suptitle('model '+'r'+args.seed+' | activation at '+str(args.activation*20)+'%')
 else:
-    fig.suptitle('model '+'r'+args.seed+'_au'+args.au+'_activation at '+str(args.activation*20)+'%')
+    fig.suptitle('model '+'r'+args.seed+' | au'+args.au+' | activation at '+str(args.activation*20)+'%')
 
 plt.show()
