ad6f2a9352288a42fa674ff1c1b183103a6321f1
Yuan Tang <hiyuantang@gmail.com>
2023-05-03 23:23:45 -0700

    Update demo.py

--- code/demo.py
+++ code/demo.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append('./code')
 import argparse
+import cv2
 import torch
 import torch.nn as nn
-from torchvision import transforms
 import numpy as np
 import argparse
 from vgg_face import *
@@ -29,7 +29,7 @@ def test_model(model, crop_frame, device):
         crop_frame = crop_frame / 255.0
         crop_frame = np.transpose(crop_frame, (2, 0, 1))
         crop_frame = np.expand_dims(crop_frame, axis=0)  # add batch dimension
-        crop_frame = torch.from_numpy(crop_frame).float()
+        crop_frame = torch.from_numpy(crop_frame).float().to(device)
         output = model(crop_frame)
         output = output * torch.FloatTensor([16] + [5]*9).to(device)
     return output
@@ -43,18 +43,11 @@ def main():
     num_ftrs = model.fc8.in_features
     model.fc8 = nn.Linear(num_ftrs, num_classes).to(device)
     model.load_state_dict(torch.load(args.mpath, map_location=device))
-    
-    data_transforms = {
-            'test': transforms.Compose([
-                transforms.ToPILImage(),
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
-            ])
-        }
 
-
+    # add face detection model
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # set up a camera window
     cv2.namedWindow('face_cap', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('face_cap', 900, 600)
     cap = cv2.VideoCapture(0)
@@ -70,15 +63,32 @@ def main():
         for (x, y, w, h) in faces:
             cv2.rectangle(frame_flip, (x, y), (x+w, y+h), (255, 0, 0), 2)
             crop_frame = frame_flip[y:y+h, x:x+w]
-
-        cv2.imshow('face_cap', frame_flip)
         
         # feed the frame into the model
         try: 
             output = test_model(model, crop_frame, device)
-            print(output)
+            output = output.flatten()
+            output_text = '|PSPI {:.2f} |au4 {:.2f} |au6 {:.2f} |au7 {:.2f} |au10 {:.2f} |au12 {:.2f} |au20 {:.2f} |au25 {:.2f} |au26 {:.2f} |au43 {:.2f}|'.format(output[0].item(), output[1].item(), output[2].item(), output[3].item(), output[4].item(), output[5].item(), output[6].item(), output[7].item(), output[8].item(), output[9].item())
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1
+            #thickness = 2
+            text_size, _ = cv2.getTextSize(output_text, font, font_scale, 2)
+            height, width, channels = frame_flip.shape
+            text_x = int((width - text_size[0]) / 2)
+            text_y = height - text_size[1] - 10
+            cv2.putText(frame_flip, output_text, (text_x, text_y), font, font_scale, (255, 255, 255), 2)
         except:
-            pass
+            output_text = 'No Face Is Detected'
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            font_scale = 1
+            #thickness = 2
+            text_size, _ = cv2.getTextSize(output_text, font, font_scale, 2)
+            height, width, channels = frame_flip.shape
+            text_x = int((width - text_size[0]) / 2)
+            text_y = height - text_size[1] - 10
+            cv2.putText(frame_flip, output_text, (text_x, text_y), font, font_scale, (255, 255, 255), 2)
+
+        cv2.imshow('face_cap', frame_flip)
 
         # Press 'Q' to quit camera
         if cv2.waitKey(1) == ord('q'):
