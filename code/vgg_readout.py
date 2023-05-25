2556266b048f13dc3657e177c29604965a68220b
Yuan Tang <hiyuantang@gmail.com>
2023-05-25 14:18:40 -0700

    Create vgg_readout.py

---
+++ code/vgg_readout.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class Readout(nn.Module):
+    
+    def __init__(self):
+        super().__init__()
+        self.fc6 = nn.Linear(512 * 7 * 7, 4096)
+        self.dropout6 = nn.Dropout(p=0.5)
+        self.fc7 = nn.Linear(4096, 4096)
+        self.dropout7 = nn.Dropout(p=0.5)
+        self.fc8 = nn.Linear(4096, 2622)
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = F.sigmoid(self.fc6(x))
+        #x = self.dropout6(x)
+        x = F.sigmoid(self.fc7(x))
+        #x = self.dropout7(x)
+        return self.fc8(x)
+
+def get_model():
+    model = Readout()
+    return model, optim.Adam(model.parameters(), lr=1e-3)
+
+def get_jacobian(net, x, noutputs):
+    x.requires_grad = True
+    y = net(x)
+    grad_params = torch.autograd.grad(y, x, create_graph=True)
+    return grad_params[0]
