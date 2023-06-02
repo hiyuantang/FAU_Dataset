fbfc0572e312295aa05f2c885256cdb51e61304f
Yuan Tang <hiyuantang@gmail.com>
2023-06-02 04:05:17 -0700

    Create launch.sh

---
+++ code/launch.sh
@@ -0,0 +1,5 @@
+python train.py --dataset_root D:/Datasets/FAU --resume D:/FAU_models/checkpoint_epoch_init.pth --seed 20 --mode 0 
+python train.py --dataset_root D:/Datasets/FAU --resume D:/FAU_models/checkpoint_epoch_init.pth --seed 21 --mode 0
+
+python train.py --dataset_root D:/Datasets/FAU --resume D:/FAU_models/checkpoint_epoch_init.pth --seed 22 --mode 0 
+python train.py --dataset_root D:/Datasets/FAU --resume D:/FAU_models/checkpoint_epoch_init.pth --seed 23 --mode 0 
